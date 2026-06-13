d1566b08aa76b85acd015d814488b2e96842901d
KULDEEPJAKHAR <kuldeepjakhar8055@gmail.com>
2026-06-14 04:18:57 +0530

    fix: remove prisma.config.ts to fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Allow localtunnel domains
+  images: {
+    domains: ["loca.lt"],
+  },
 };
 
 export default nextConfig;
